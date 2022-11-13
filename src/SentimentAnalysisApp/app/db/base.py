018a6396107caf81108aede04085c59aaf872d89
Fergons <fergons777@gmail.com>
2022-11-13 15:50:22 +0100

    added app.models.source import to base so it is possible to autogenerate migrations

--- src/SentimentAnalysisApp/app/db/base.py
+++ src/SentimentAnalysisApp/app/db/base.py
@@ -2,7 +2,8 @@
 # imported by Alembic
 from app.db.base_class import Base  # noqa
 from app.models.user import User # noqa
-from app.models.game import Game  # noqa
+from app.models.game import Game, Category, GameCategory  # noqa
 from app.models.aspect import Aspect  # noqa
 from app.models.review import Review  # noqa
 from app.models.reviewer import Reviewer  # noqa
+from app.models.source import Source, GameSource  # noqa
