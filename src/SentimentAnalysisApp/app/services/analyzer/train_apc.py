e328068a1645f70aa95bfb01d8903c4a82605746
Fergons <fergons777@gmail.com>
2022-12-13 20:16:42 +0100

    fix train_apc.py

--- src/SentimentAnalysisApp/app/services/analyzer/train_apc.py
+++ src/SentimentAnalysisApp/app/services/analyzer/train_apc.py
@@ -17,23 +17,9 @@ config.cache_dataset = True
 dataset = DatasetItem("1111.game_reviews")
 
 aspect_extractor = APC.APCTrainer(config=config,
-                                  from_checkpoint='fast_lcf_atepc_1111.game_reviews_cdw_apcacc_62.35_apcf1_48.51_atef1_78.29',
+                                  from_checkpoint='',
                                   dataset=dataset,
                                   checkpoint_save_mode=ModelSaveOption.SAVE_MODEL_STATE_DICT,
                                   auto_device=True,
                                   load_aug=False
                                   )
-
-# atepc_examples = [
-#     'Velmi dobra hra, chytlava :-) ziadne problemy s optimalizaciou ani na ziadne bugy som nenatrafil. Ked to hrate s intel pentium tak sa nepiste recenzie typu : Mam frame dropy a laguje mi to...',
-#     'Určitě doporučuji ! Vývojáři se snaží s hrou pracovat a posouvat ji dále. Za mě better and better. GJ !!',
-#     'Celkem vklidu hra s velmi dobrou grafikou, ale špatnou optimalizací.  :)',
-#     "Bezva hra, chce to cvik ale když to baví tak se dá vydržet než si to vychytáte.Doporučuji sluchátka s mikrofonem a umět trochu anglicky, pak je hra o 100 zábavnější.",
-#     "Hra je super, potrebuje ešte pár optimalizácií, ale už teraz je to najlepší Battle Royale aký som kedy hral.PS: Je to jediný Battle Royale, čo som kedy hral :D"
-#     ]
-#
-# aspect_extractor.batch_predict(atepc_examples,  #
-#                                save_result=True,
-#                                print_result=True,  # print the result
-#                                pred_sentiment=True,  # Predict the sentiment of extracted aspect terms
-#                                )
